eb30f0cff8ee5f2ace6b1bf43ab7222a566dfb94
Juanse1595 <3309@holberstonschool.com>
2022-01-29 11:03:21 -0500

    FIX autoformating

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,7 +5,9 @@ from flask import Flask
 
 
 app = Flask(__name__)
-@app.route("/", strict_slashes = False)
+
+
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
     """
     Return value for root directory
