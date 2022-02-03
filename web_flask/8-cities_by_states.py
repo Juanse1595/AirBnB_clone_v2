2ee91ed6419b30401879226931db9f744bbfb357
Juanse1595 <3309@holberstonschool.com>
2022-02-02 20:29:39 -0500

    FIX 8th identation html

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
-"""8-cities_by_states module
-Starts a Flask web application"""
+"""Script that starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
+@app.route("/states_list")
 def states_list():
-    '''Sends objects to html template'''
-    state_objs = storage.all(State).values()
-    return render_template('7-states_list.html', state_objs=state_objs)
+    """Render the list of the states"""
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
 
 
 @app.route('/cities_by_states')
-def cities_by_states_list():
-    '''Sends objects to html template'''
-    state_objs = storage.all(State).values()
-    return render_template('8-cities_by_states.html', state_objs=state_objs)
+def cities_by_states():
+    """Render the list of the states"""
+    states = storage.all(State).values()
+    return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
-def remove_session(self):
-    '''Removes session'''
+def teardown_db(db):
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
