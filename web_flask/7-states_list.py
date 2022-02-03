5ca0ba4fed8ccde7eca4a5c80a58b01b8f7f23a4
Juanse1595 <3309@holberstonschool.com>
2022-02-02 19:42:29 -0500

    FIX test 2, had an error in 7html stateS_objs

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-"""Module that contains a script that starts a Flask web application
-"""
-
+"""7-states_list module
+Starts a Flask web application"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route("/states_list")
+@app.route('/states_list')
 def states_list():
-    """Render the list of the states"""
-    states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
+    '''Sends objects to html template'''
+    state_objs = storage.all(State).values()
+    return render_template('7-states_list.html', state_objs=state_objs)
 
 
 @app.teardown_appcontext
-def teardown_db(self):
-    """Remove the current SQLAlchemy Session"""
+def remove_session(self):
+    '''Removes session'''
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
