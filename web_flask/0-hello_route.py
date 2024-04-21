6e72904def1ecb3ab5be6cd4365d40ad1bef90ed
Juanse1595 <jspbescos15@gmail.com>
2024-04-21 11:57:41 -0500

    UPD 0th adding comments to the file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""
+0th file with a basic Flask app
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    """
+    Root route that returns 'Hello HBNB!'
+    """
     return "Hello HBNB!"
 
 if __name__ == '__main__':
