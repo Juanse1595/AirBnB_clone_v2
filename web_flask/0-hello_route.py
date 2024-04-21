a9268108b8fcdee185796012049a723553628a30
Juanse1595 <jspbescos15@gmail.com>
2024-04-21 11:58:45 -0500

    UPD 0th fixed pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,12 +6,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     """
     Root route that returns 'Hello HBNB!'
     """
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
