0d51c233c7d8b8a04da3c0378664f934e747338a
Juanse1595 <jspbescos15@gmail.com>
2024-04-22 15:18:18 -0500

    UPD 0th task file upload

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+0th task file with a Flask app
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_hbnb():
+    """Root route that returns 'Hello HBNB!'
+
+    Returns:
+        str: returns 'Hello HBNB!'
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
